6c7bfb66d0efdc06346934517d13d966e7659c23
pognet <pogo@interbit.pl>
2018-10-26 12:16:39 +0200

    refactor

--- main.c
+++ main.c
@@ -1,29 +1,42 @@
 #include <stdio.h>
 
-int main() {
-    int liczba;
-    int licznik_petli = 0;
-    char znak = 'n';
+int zrob_nieparzysta(int liczba) {// zrób nieparzystą
+    if (!(liczba % 2)) --liczba;
+    return liczba;
+}
 
+int szukaj_liczby(int liczba) {
+    for (; !(liczba % 3) || !(liczba % 5) || !(liczba % 7); liczba -= 2) {}
+    printf("znaleziona liczba: %d\n", liczba);
+    return liczba;
+}
+
+void program_glowny() {
+    int liczba;
+
+    printf("Podaj liczbę: ");
+    scanf(" %d", &liczba);
+
+    liczba = zrob_nieparzysta(liczba);
+
+    liczba = szukaj_liczby(liczba);
+}
+
+void wyswietl_info() {
     printf("Program liczy największą liczbę mniejszą od\n");
     printf("liczby podanej przez użytkownika, która\n");
     printf("jest niepodzielna przez 2, 3, 5, 7.\n\n");
+}
+
+int main() {
+     char znak = 'n';
+
+    wyswietl_info();
 
     do {
-        printf("Podaj liczbę: ");
-        scanf(" %d", &liczba);
+        program_glowny();
 
-        // zrób nieparzystą
-        if (!(liczba % 2)) --liczba;
-
-        for (; !(liczba % 3) || !(liczba % 5) || !(liczba % 7); liczba -= 2) {
-            licznik_petli++;
-        }
-        printf("znaleziona liczba: %d\n", liczba);
-
-        printf("\n");
-        printf("liczba pętli wyniosła: %d\n", licznik_petli);
-
+        // każdy znak kończy / małe lub duże 't' kontynuuje
         printf("\nCzy liczyć jeszcze raz? (t/n) ");
         scanf(" %c", &znak);
     } while (znak == 't' || znak == 'T');
