0933fcf6e4fd68a2a7ec29ab99cf5c6e7d0d7772
pognet <pogo@interbit.pl>
2018-10-26 12:02:54 +0200

    petle-do-while

---
+++ cmake-build-debug/CMakeFiles/petle.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/petle.dir/main.c.o"
+  "petle.pdb"
+  "petle"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/petle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
